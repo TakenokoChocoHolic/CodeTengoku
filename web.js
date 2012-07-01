e32332f80428743c477468bd4e88f1a7b7720984
GreatAS <akira.radiant@gmail.com>
2012-07-01 17:09:28 +0900

    maybe modified web.js

--- web.js
+++ web.js
@@ -3,6 +3,10 @@ app.get('/problem_set', function(req, res) {
     res.render('index.ejs', {locals:{mes:mes}});
 });
 
+app.get('/problem', function(req, res) {
+    res.render('problem.ejs', {locals:{mes:''}});
+});
+
 app.get('/funny_api/', function(req, res) {
     res.render('funny_api.ejs', {locals:{mes:''}});
 });
